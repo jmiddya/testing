2f40f831b84271bb6df13e49eb94c00505fb2b4a
J Middya <jmiddya.mca@gmail.com>
2017-02-28 22:45:40 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,68 @@
+var express = require('express')
+var app = express()
+
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static(__dirname + '/public'))
+
+//////////JM : START//////////
+var location = 'Kolkata'
+
+var getWeather = function ( location) {
+  var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + location + '&appid=f953e7b081a49dc14a56671ffa303848';
+  return fetch(url, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+    }
+  })
+    .then(rsp => {
+      var res = rsp.json();
+      return res;
+    })
+    .then(json => {
+      if (json.error && json.error.message) {
+        throw new Error(json.error.message);
+      }
+      return json;
+    });
+}
+
+try {
+  // if running from repo
+  Wit = require('../').Wit;
+  interactive = require('../').interactive;
+} catch (e) {
+  Wit = require('node-wit').Wit;
+  interactive = require('node-wit').interactive;
+}
+//////////JM : END//////////
+
+app.get('/', function(request, response) {
+  //response.send('Hello World!')
+  getWeather(location).then(weatherJson => {
+		  var weatherDetails = '';
+		  	
+		  var temp = weatherJson.main.temp - 273.15;
+		  weatherDetails = weatherDetails + 'Current temperature : ' + temp + ' C';
+			
+		  var humid = weatherJson.main.humidity;
+		  weatherDetails = weatherDetails + ', Humidity : ' + humid + '%';
+				
+	      var cloud = weatherJson.clouds.all;
+		  weatherDetails = weatherDetails + ', Cloud : ' +cloud + '%';
+			
+		  var desc = weatherJson.weather[0].description;
+		  weatherDetails = weatherDetails + ', Overall weather : ' + desc;
+			
+		  weatherDetails = weatherDetails + ' in ' + location;
+		  
+          console.log( weatherDetails );
+		  
+		  response.send(weatherDetails);
+        })
+})
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
